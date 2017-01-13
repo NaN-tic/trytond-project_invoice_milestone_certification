dd4c3f8ac7c4a2b3f5922daaf5d4bf38fecb3b46
?ngel ?lvarez <angel@nan-tic.com>
2017-01-13 16:31:55 +0100

    initial commit

---
+++ __init__.py
@@ -0,0 +1,11 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+
+from trytond.pool import Pool
+from . import work
+
+
+def register():
+    Pool.register(
+        work.Certification,
+        module='project_invoice_milestone_certification', type_='model')
---
+++ work.py
@@ -0,0 +1,23 @@
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
+from trytond.pool import PoolMeta, Pool
+
+__all__ = ['Certification']
+
+
+class Certification:
+    __name__ = 'project.certification'
+    __metaclass__ = PoolMeta
+
+    @classmethod
+    def confirm(cls, certifications):
+        pool = Pool()
+        Milestone = pool.get('project.invoice_milestone')
+        super(Certification, cls).confirm(certifications)
+        milestones = []
+        for cert in certifications:
+            for x in cert.lines:
+                for y in x.work.milestones:
+                    milestones.append(y)
+            milestones += cert.work.milestones
+        Milestone.check_trigger(milestones)
